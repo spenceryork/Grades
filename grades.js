2a68fb4f05092f336e4ddfe1d7bad5b1b94dd6a4
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2017-11-29 09:07:15 -0600

    turned this into correct if else statement

--- grades.js
+++ grades.js
@@ -9,20 +9,15 @@ let fGrade = [];
 for (i = 0; i < scores.length; i++) {
     if ((scores[i]) >= 91) {
         aGrade.push(scores[i]);
-    }
-    if ((scores[i]) <= 90 && (scores[i]) >= 81) {
+    } else if ((scores[i]) <= 90 && (scores[i]) >= 81) {
         bGrade.push(scores[i]);
-    }
-    if ((scores[i]) <= 80 && (scores[i]) >= 71) {
+    } else if ((scores[i]) <= 80 && (scores[i]) >= 71) {
         cGrade.push(scores[i]);
-    }
-    if ((scores[i]) <= 70 && (scores[i]) >= 61) {
+    } else if ((scores[i]) <= 70 && (scores[i]) >= 61) {
         dGrade.push(scores[i]);
-    }
-    if ((scores[i]) <= 60) {
+    } else {
         fGrade.push(scores[i]);
-    }
-    
+    }   
 }
 console.log(aGrade.length + " students got an A");
 console.log(bGrade.length + " students got a B");
